f9df51cef5810701c7bfdc760fc1a4e71e17f213
jhoon <jhoneduardsuarezsalazar6@gmail.com>
2026-04-07 21:13:48 -0500

    front inicial

---
+++ frontend-android/EventraMobile/app/src/main/java/com/eventra/mobile/activities/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.eventra.mobile.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.eventra.mobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
